c468b760f667df827604159bdaab1180e2cf87b4
kaiyin <kaiyin@gaotengasset.com>
2021-03-01 14:26:59 +0800

    feat(cache): 忽略掉缓存文件

--- command/init.js
+++ command/init.js
@@ -8,7 +8,7 @@ const init_action = async (option) => {
         { type: 'list', name: 'project_type', message: '请选择项目类型！', choices: ['server', 'web'] },
     ]);
     const start_time = (new Date()).getTime();
-    await utils.downloadFile('', '从远程仓库拉取模板');
+    await utils.downloadFile('https://github.com/cherishs001/cli/releases/download/1.0.0/server.zip', '从远程仓库拉取模板');
 
     const end_time = (new Date()).getTime();
     console.log('\033[42;30m DONE \033[40;32m Compiled successfully in ' + (end_time - start_time) + 'ms\033[0m');
