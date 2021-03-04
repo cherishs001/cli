daca636312226b120b27912a607ead991d7fee54
kaiyin <kaiyin@gaotengasset.com>
2021-03-04 11:17:11 +0800

    feat(fix): fix命令增加检查dev命令是否存在

--- command/fix.js
+++ command/fix.js
@@ -1,4 +1,5 @@
 const utils = require('./utils');
+const fs = require('fs');
 
 const fix_action = async () => {
     // 删除node_moudle后，重新安装
@@ -6,6 +7,12 @@ const fix_action = async () => {
     const pb = new utils.ProgressBar('正在修复...', 50); 
     utils.deleteFolder('./node_modules', {pb, nums: nums + (nums / 14)})
     await utils.npm_install('./');
+    pb.render({ completed: 99, total: 100 });
+    // 检查package.json里面有没有dev命令，并增加这个命令
+    const package = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+    if (!package.scripts.hasOwnProperty('dev')) {
+        package.scripts.dev = 'ts-node index.ts';
+    }
     pb.render({ completed: 100, total: 100 });
 }
 
